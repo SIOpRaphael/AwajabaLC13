bb0bf4c2a562503cb2be5683d09646742af5276a
raphael <raphael.pichegru.sio@gmail.com>
2026-09-07 10:17:09 +0200

    creation constructeur

--- Repas.java
+++ Repas.java
@@ -5,4 +5,10 @@ private class Repas {
 	private int numero;
 	private LocalDate date
 	
+	public Repas (int numero, LocalDate date){
+		super();
+		this.numero = numero;
+		this.date = date;
+	}
+	
 }
